10a263d742972ccdb6b065e022480c145e06b5cc
roman <sk.trac@yandex.ru>
2021-03-19 14:48:47 +0300

    * added skins to bg table

--- src/classes/renderers/bg-table-renderer.js
+++ src/classes/renderers/bg-table-renderer.js
@@ -1,4 +1,16 @@
 import {GAME_ENGINE_ACTIONS} from "../mediators/game-mediator.js";
+import { CELL_TYPES as CT } from '../cells/cell-types.js';
+
+
+const iSkin = {
+    [CT.EMPTY]: '<td class="game__table__td game__table__td--ground"></td>',
+    [CT.WALL]: '<td class="game__table__td game__table__td--box"></td>',
+    [CT.TARGET]: '<td id="default-environment-place" class="game__table__td game__table__td--environment"></td>',
+    [CT.BOX_ON_EMPTY]: '<td id="default-brown-box-place" class="game__table__td game__table__td--brown-box"></td>',
+    [CT.BOX_ON_TARGET]: '<td class="game__table__td game__table__td--green-ground"></td>',
+    [CT.PLAYER_ON_EMPTY]: '<td id="default-player-place" class="game__table__td game__table__td--ground game__table__td--player game__table__td--player-default"></td>',
+    [CT.PLAYER_ON_TARGET]:'<td id="default-player-place" class="game__table__td game__table__td--ground game__table__td--player game__table__td--player-default saturate"></td>'
+}
 
 export class BgTableRenderer {
     constructor(rootElementSelector, levelsManager, mediator) {
@@ -29,6 +41,8 @@ export class BgTableRenderer {
     }
 
     getHtml() {
+        console.log(this.currentLevelStructure);
+        return;
         return `<div class="main">
                     <header class="header">
                         <nav class="header__nav">
