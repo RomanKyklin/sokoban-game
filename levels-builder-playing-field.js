6c4d909594459a5f2cc4104bb0bbe0e8037ba511
roman <sk.trac@yandex.ru>
2021-03-22 12:17:12 +0300

    * levels builder playing field blueprint

---
+++ levels-builder-playing-field.js
@@ -0,0 +1,15 @@
+import {PlayingField} from "./src/classes/playing-field.js";
+
+export class LevelsBuilderPlayingField extends PlayingField {
+    constructor() {
+        super();
+    }
+
+    getFirstLevelStructure() {
+
+    }
+
+    getSecondLevelStructure() {
+
+    }
+}
